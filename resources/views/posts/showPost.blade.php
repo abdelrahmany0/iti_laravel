3e809106dceb9792ca983f049feaf3fe85ce0b47
Abdelrahman Yasser <maxtor11.aa@gmail.com>
2021-03-21 16:07:27 +0200

    adding post creator to view

--- resources/views/posts/showPost.blade.php
+++ resources/views/posts/showPost.blade.php
@@ -5,18 +5,38 @@ show post
 @section('content')  
 <div class="container mt-4">
     <div class="card">
-        <div class="card-header">
+        <div class="card-header bg-secondary">
             <h6>Post Info</h6>
-            <h6 style="color: forestgreen;">ID: {{ $post['id'] }}</h6>
+            <h6 class="text-dark">ID: {{ $post['id'] }}</h6>
         </div>
         <div class="card-body">
-            <h5 class="card-title text-bold">Title:</h5>
-            <p class="inline">{{ $post['title'] }}</p>
+            <div class="d-flex">
+                <h5 class="card-title text-bold">Title:-</h5>
+                <p class="inline">{{ $post['title'] }}</p>
+            </div>
             <h4 class="card-title">Description:</h4>
             <p class="card-text">{{ $post['description'] }}</p>
-            <h4 class="card-title">Created At:</h4>
-            <p class="card-text">{{ $post['created_at'] }}</p>
+            <div class="d-flex align-items-center">
+                <h4 class="card-title">Created At:-</h4>
+                <p class="card-text">{{ $post['created_at'] }}</p>
+            </div>
         </div>
-        </div>
+    </div>
+    <div class="card mt-2">
+        <div class="card-header bg-secondary">
+                <h6>Post Creator Info</h6>
+                <h6 class="text-dark">ID: {{ $post->user->id }}</h6>
+            </div>
+            <div class="card-body">
+                <div class="d-flex">
+                    <h5 class="card-title text-bold">Name:-</h5>
+                    <p class="inline">{{ $post->user->name }}</p>
+                </div>
+                <div class="d-flex">
+                    <h4 class="card-title">Email:-</h4>
+                    <p class="card-text">{{ $post->user->email }}</p>
+                </div>
+            </div>
+    </div>
 </div>
 @endsection
