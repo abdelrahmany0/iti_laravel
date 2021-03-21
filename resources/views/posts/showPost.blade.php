8cb5195cb8c110ceeb8d62cbb7568d7ee228c54c
Abdelrahman Yasser <maxtor11.aa@gmail.com>
2021-03-21 15:15:31 +0200

    adding created at in 'showPost'

--- resources/views/posts/showPost.blade.php
+++ resources/views/posts/showPost.blade.php
@@ -4,13 +4,15 @@
     <div class="card">
         <div class="card-header">
             <h6>Post Info</h6>
-            <h6>Post ID:{{ $post['id'] }}</h6>
+            <h6 style="color: forestgreen;">ID: {{ $post['id'] }}</h6>
         </div>
         <div class="card-body">
             <h5 class="card-title text-bold">Title:</h5>
             <p class="inline">{{ $post['title'] }}</p>
-            <h4 class="card-title">Descirption:</h4>
+            <h4 class="card-title">Description:</h4>
             <p class="card-text">{{ $post['description'] }}</p>
+            <h4 class="card-title">Created At:</h4>
+            <p class="card-text">{{ $post['created_at'] }}</p>
         </div>
         </div>
 </div>
